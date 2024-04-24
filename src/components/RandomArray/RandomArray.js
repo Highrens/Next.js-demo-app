2af7c13d44ad47bd23e60b8de9ebbdd9db0db1a8
Sergei Pankov <sergeipankov98@mail.ru>
2024-04-24 18:10:10 +0400

    ArrayLenght fix

--- src/components/RandomArray/RandomArray.js
+++ src/components/RandomArray/RandomArray.js
@@ -4,7 +4,7 @@ import "./RandomArray.css";
 
 export default function RandomArray(props) {
   const [trueCount, setTrueCount] = useState(1);
-  const [sum, setSum] = useState([0, 0, 0]);
+  const [sum, setSum] = useState([0, 0, 0, 0, 0]);
   const [realSum, setRealSum] = useState(0);
 
   useEffect(() => {
@@ -23,7 +23,7 @@ export default function RandomArray(props) {
     arr[number] = int + 1;
     console.log(arr);
     setSum(arr);
-    let i = arr[0] + arr[1] + arr[2];
+    let i = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
     setRealSum(i);
     console.log(i);
   }
